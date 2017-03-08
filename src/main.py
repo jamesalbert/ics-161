14ba50229dd32facc1780ce62ad725f46e13a392
jamesalbert <jalbert1@uci.edu>
2017-03-07 22:38:52 -0800

    ability to gen docs and pep8

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+"""
+The greatest library that ever lived!!!
+"""
+
+# === THE minus function ===
+
+
 def minus(x, y):
+    """
+    This function uses quantum mechanics to determine the difference between
+    the subtrahend and minuend.
+    """
     return x - y
